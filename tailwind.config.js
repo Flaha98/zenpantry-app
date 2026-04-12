c8d6f6bcebbdf60c67bb873a35ebb1e8dbdb35e8
Flavia <flaviaha.98@gmail.com>
2026-04-11 23:39:46 -0400

    chore: initialize Angular project with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        sage: '#7C9E87',
+        cream: '#FAF7F2',
+        charcoal: '#2D2D2D',
+        dark: {
+          bg: '#1E1E2E',
+          card: '#2A2A3E',
+        }
+      }
+    }
+  },
+  plugins: []
+}
